69a387904b03a973c2aee257c0178c2924207708
David Vloedman <djvloedman@gmail.com>
2020-03-11 11:16:06 -0700

    Plugins and fleshed out template

---
+++ portolfio-grayscale/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteTitle: 'David Vloedman', // <title>
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/david-vloedman/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'David Vloedman',
+  subHeading: 'Full Stack Web Developer',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/david-vloedman',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/davidvloedman/',
+    },
+  ],
+  email: 'djvloedman@gmail.com',
+  phone: '541-797-3660',
+  address: '2600 NE Forum Dr Apt 49 Bend, OR',
+};
